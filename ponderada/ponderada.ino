814ba51d65cf184d44ab1233b2b9496a8eb54cf5
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2023-04-21 20:52:11 -0300

    Joao Victor Montagna
    IF ELSE ALTURA FEITO

---
+++ ponderada/ponderada.ino
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+void setup() {
+
+}
+//Deixa esse ou substitui?
+void loop() {  
+    
+    String nome;
+    Serial.print("Digite o seu nome: ");
+    while (!Serial.available());
+    nome = Serial.readStringUntil('\n');
+    nome.trim(); // Remove os caracteres de quebra de linha }
+
+
+
+//Joao
+  void loop(){
+    if (altura>= 1.80){
+      Serial.println("Você é alto.");
+    }
+    else{
+      Serial.println("Você não é tao alto");
+    }
+    Serial.println(nome);
+    delay(5000); //5 segundos refaz o loop
+  
+  }
